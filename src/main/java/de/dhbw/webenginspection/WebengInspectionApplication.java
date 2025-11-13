bdd0a2714018e57e052373af4827ee940cedf384
lucas <sacool@web.de>
2025-11-13 21:18:45 +0100

    Added Spring Boot skeleton with hello endpoint

---
+++ src/main/java/de/dhbw/webenginspection/WebengInspectionApplication.java
@@ -0,0 +1,13 @@
+package de.dhbw.webenginspection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebengInspectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebengInspectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dhbw/webenginspection/WebengInspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbw.webenginspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebengInspectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
